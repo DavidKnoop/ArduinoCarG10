214f932ebe463d80106e07d0e5a8ad6cd7db90fd
FortuneGER <162564301+FortuneGER@users.noreply.github.com>
2024-03-14 10:19:50 +0100

    grundprogrsmm

--- arduino_car_Grundprogramm.ino
+++ arduino_car_Grundprogramm.ino
@@ -2,18 +2,29 @@
 #include <IRremote.h>
 #include <Servo.h>
 //Pin festlegung:
+//-----------Linetracker-------------------------
 #define LT_R 10 //Line Tracker ((R)echts Tracker)
 #define LT_M 4  //Line Tracker ((M)ittel Tracker)
 #define LT_L 2  //Line Tracker ((L)inks Tracker)
-
+//-----------Ultraschallsensor-------------------
+#define ECHO_PIN A4 //Signal empfangen
+#define TRIG_PIN A5 //Signal senden
+//-----------Motor-Rechts------------------------
 #define ENA 3   //oder Pin 5 Car-Speed
-#define ENB 6   //Car-Speed
 #define IN1 7   //Antrieb
 #define IN2 8   //Antrieb
+//-----------Motor-Links-------------------------
+#define ENB 6   //Car-Speed
 #define IN3 9   //Antrieb
 #define IN4 11  //Antrieb
-#define LED       13        //LED pin
+//-----------LED---------------------------------
+#define LED 13  //LED pin
+//-----------Infrarot----------------------------
 #define RECV_PIN  12 //Infrarot Sensor
+//-----------Bluetooth Modul---------------------
+#define RX 0
+#define TX 1
+//-----------------------------------------------
 #define carSpeed 150
 
 //Variablen festlegen:
