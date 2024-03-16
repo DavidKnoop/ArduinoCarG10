351ad5090910c9709a39a99bc5532324f161d20d
David <101398520+DavidKnoop@users.noreply.github.com>
2024-03-16 20:48:35 +0100

    optimize functions

--- Arduino Bauteile/L298N/L298n.ino
+++ Arduino Bauteile/L298N/L298n.ino
@@ -1,9 +1,7 @@
 void carspeed(int speedA = DEFAULT_SPEED, int speedB = DEFAULT_SPEED)
 {
-  if (speedA < 0 || speedA > 50)
-    speedA = DEFAULT_SPEED;
-  if (speedB < 0 || speedB > 50)
-    speedB = DEFAULT_SPEED;
+  if (speedA < 0 || speedA > 50) speedA = DEFAULT_SPEED;
+  if (speedB < 0 || speedB > 50) speedB = DEFAULT_SPEED;
 
   analogWrite(ENA, speedA);
   analogWrite(ENB, speedB);
@@ -26,46 +24,50 @@ void backward(int SA = DEFAULT_SPEED, int SB = DEFAULT_SPEED)
   digitalWrite(IN4, LOW);
 }
 
-void curve_right_forward(int SA = DEFAULT_SPEED, int SB = DEFAULT_C_SPEED)
+void curve(int SS = DEFAULT_SPEED, int LS = DEFAULT_C_SPEED, const char *direction) // SNBE: Should Not Be Empty!!!
 {
-  carspeed(SB, SA);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  if (strcmp(direction, "right") == 0)
+  {
+    carspeed(SS, LS);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+  else if (strcmp(direction, "left") == 0)
+  {
+    carspeed(LS, SS);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  } else ERROR();
 }
-void curve_left_forward(int SA = DEFAULT_C_SPEED, int SB = DEFAULT_SPEED)
+void curve_backward(int LS = DEFAULT_C_SPEED, int SB = DEFAULT_SPEED, const char *direction)
 {
-  carspeed(SA, SB);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-}
-void curve_left_backward(int SA = DEFAULT_C_SPEED, int SB = DEFAULT_SPEED)
-{
-  carspeed(SA, SB);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-void curve_right_backward(int SA = DEFAULT_SPEED, int SB = DEFAULT_C_SPEED)
-{
-  carspeed(SA, SB);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
+  if (strcmp(direction, "right") == 0)
+  {
+    carspeed(LS, SS);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else if (strcmp(direction, "left") == 0)
+  {
+    carspeed(SS, LS);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else ERROR();
 
-void cycle_to(const char *direction = DEFAULT_DIRECTION, int LS = DEFAULT_CYCLE_LS, int SS = DEFAULT_CYCLE_SS)
+void cycle_to(const char *direction = DEFAULT_DIRECTION, int LS = DEFAULT_CYCLE_LS, int SS = DEFAULT_CYCLE_SS, long myTimout = 0)
 {
   if (strcmp(direction, "left") == 0)
   {
-    myTimer = millis(); //setzte myTimer auf aktuelle laufzeit
+    myTimer = millis();   // setzte myTimer auf aktuelle laufzeit
     long myTimeout = 220; // soviele millis soll das car sich drehen
-    
+
     while (millis() < myTimer + myTimeout) //(myTimout == 1000) Nun wird geschaut ob meine aktuelle Laufzeit(millis()) noch im bereich meiner gewünschten zeit ist, die die funktion durchlaufen soll
     {
       carspeed(LS, SS);
@@ -77,7 +79,7 @@ void cycle_to(const char *direction = DEFAULT_DIRECTION, int LS = DEFAULT_CYCLE_
   }
   else if (strcmp(direction, "right") == 0)
   {
-    myTimer = millis(); //setzte myTimer auf aktuelle laufzeit
+    myTimer = millis();   // setzte myTimer auf aktuelle laufzeit
     long myTimeout = 220; // soviele millis soll das car sich drehen
 
     while (millis() < myTimer + myTimeout) //(myTimout == 1000) Nun wird geschaut ob meine aktuelle Laufzeit(millis()) noch im bereich meiner gewünschten zeit ist, die die funktion durchlaufen soll
@@ -91,7 +93,7 @@ void cycle_to(const char *direction = DEFAULT_DIRECTION, int LS = DEFAULT_CYCLE_
   }
   else if (strcmp(direction, "back") == 0)
   {
-    myTimer = millis(); //setzte myTimer auf aktuelle laufzeit
+    myTimer = millis();   // setzte myTimer auf aktuelle laufzeit
     long myTimeout = 717; // soviele millis soll das car sich drehen
 
     while (millis() < myTimer + myTimeout) //(myTimout == 1000) Nun wird geschaut ob meine aktuelle Laufzeit(millis()) noch im bereich meiner gewünschten zeit ist, die die funktion durchlaufen soll
@@ -103,11 +105,11 @@ void cycle_to(const char *direction = DEFAULT_DIRECTION, int LS = DEFAULT_CYCLE_
       digitalWrite(IN4, LOW);
     }
   }
-  else if (strcmp(direction, DEFAULT_DIRECTION)==0)
+  else if (strcmp(direction, DEFAULT_DIRECTION) == 0)
   {
-    myTimer = millis(); //setzte myTimer auf aktuelle laufzeit
+    myTimer = millis();    // setzte myTimer auf aktuelle laufzeit
     long myTimeout = 2300; // soviele millis soll das car sich drehen
-    
+
     while (millis() < myTimer + myTimeout) //(myTimout == 1000) Nun wird geschaut ob meine aktuelle Laufzeit(millis()) noch im bereich meiner gewünschten zeit ist, die die funktion durchlaufen soll
     {
       carspeed(255, 255);
@@ -116,48 +118,56 @@ void cycle_to(const char *direction = DEFAULT_DIRECTION, int LS = DEFAULT_CYCLE_
       digitalWrite(IN3, HIGH);
       digitalWrite(IN4, LOW);
     }
-  } else { // Gedacht als physischer Error (Falls ein falscher wert gegeben wird)
-    myTimer = millis();
-    while(millis() >= myTimer)//dar ich zuvor myTimer auf den gleichen wert wie millis() gesetzt habe, ist millis() sowieso größer(oder zuanfang noch gleich)
-    {
-      carspeed(255, 255);
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, HIGH);
-    }
-  }
+  } else ERROR();
 }
 
-void HARD_RIGHT(int SA = DEFAULT_SPEED)
+void HARD(int SA = DEFAULT_SPEED const char *direction)
 {
-  carspeed(SA, 0);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+  if (strcmp(direction, "right") == 0)
+  {
+    carspeed(SA, 0);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  } else if (strcmp(direction, "left") == 0)
+  {
+    carspeed(0, SB);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  } else ERROR();
 }
-void HARDBACK_RIGHT(int SA = DEFAULT_SPEED)
-{
-  carspeed(SA, 0);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
 }
-void HARDBACK_LEFT(int SB = DEFAULT_SPEED)
+void HARDBACK(int SA = DEFAULT_SPEED)
 {
-  carspeed(0, SB);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  if (strcmp(direction, "right") == 0)
+  {
+    carspeed(SA, 0);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  } else if (strcmp(direction, "left") == 0)
+  {
+    carspeed(0, SB);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else ERROR();
 }
-void HARD_LEFT(int SB = DEFAULT_SPEED)
+
+void ERROR() // Gedacht als physischer Error (Falls ein falscher wert gegeben wird)
 {
-  carspeed(0, SB);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  myTimer = millis();
+  while (millis() >= myTimer) // dar ich zuvor myTimer auf den gleichen wert wie millis() gesetzt habe, ist millis() sowieso größer(oder zuanfang noch gleich)
+  {
+    carspeed(255, 255);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
 }
